ba70cfee4955605ad51a936c857bd0a610f000d1
ingoswaldo <oswaldo.montess@cecar.edu.co>
2022-06-16 10:12:15 -0500

    Removed some actions and views in the comments.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :authors, only: :index
   resources :books, only: [:index, :show] do
-    resources :comments
+    resources :comments, only: :index
   end
   resources :tags, only: :index
   resources :users, except: [:index, :destroy]
