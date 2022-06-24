655eb480c6da8f87a0a2cff29f34c9aa640a05da
ingoswaldo <oswaldo.montess@cecar.edu.co>
2022-06-24 09:42:17 -0500

    Changed the author index view layout. Added the author show view. Changed the book card layout.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,10 @@
 class AuthorsController < ApplicationController
   def index
-    @authors = Author.order('name')
+    @books = Book.joins(:author).merge(Author.order(name: :asc)).order('name').active.page params[:page]
+  end
+
+  def show
+    @author = Author.find params[:id]
+    @books = @author.books.order('name').active.page params[:page]
   end
 end
